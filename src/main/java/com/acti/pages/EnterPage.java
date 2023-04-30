db7b37484dabb9e7f8a350cdc5263578c753836f
Sudheer <Sudheer@10.0.0.4>
2023-04-30 02:04:43 -0700

    Added Author Details to Enter Page

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -2,6 +2,14 @@ package com.acti.pages;
 
 import com.acti.base.DriverScript;
 
+/*
+ * Name: Login Page
+ * Description : Manages the Locators and Methods of LoginPage 
+ * Developed By: Shantosh
+ * Reviewed By : Radhika 
+ * Date Reviewed: 04/27/2023
+ */
+
 public class EnterPage extends DriverScript{
 	
 	// **********Page Locators*******************
