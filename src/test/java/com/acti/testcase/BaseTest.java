5b5a3c76fcdf0262a22388177158448f288bb1fa
Sudheer <Sudheer@10.0.0.4>
2023-04-30 02:43:18 -0700

    Initialized and Created new object for EnterPage class in BaseTest file

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -4,11 +4,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.base.DriverScript;
+import com.acti.pages.EnterPage;
 import com.acti.pages.LoginPage;
 
 public class BaseTest extends DriverScript{
 	
 	LoginPage lp;
+	EnterPage ep;
 
 	@BeforeMethod
 	public void preTest() 
@@ -16,6 +18,7 @@ public class BaseTest extends DriverScript{
 
 		initApplication();
 		lp = new LoginPage();
+		ep = new EnterPage();
 	}
 
 	@AfterMethod
